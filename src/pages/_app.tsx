5c01264a1f142ac789d5d33ea7901d55622f6774
LogicDX342 <1500640573@qq.com>
2023-02-15 12:54:25 +0800

    Add Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -63,6 +63,7 @@ import * as Icons from '@fortawesome/free-brands-svg-icons'
 import type { AppProps } from 'next/app'
 import NextNProgress from 'nextjs-progressbar'
 import { appWithTranslation } from 'next-i18next'
+import { Analytics } from '@vercel/analytics/react';
 
 // import all brand icons with tree-shaking so all icons can be referenced in the app
 const iconList = Object.keys(Icons)
@@ -124,6 +125,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <NextNProgress height={1} color="rgb(156, 163, 175, 0.9)" options={{ showSpinner: false }} />
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
